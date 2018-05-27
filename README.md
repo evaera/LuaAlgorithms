56db2ae165095f7861c9a87d6782b653f63fb04f
Eryn Lynn <eryn@eryn.io>
2018-05-26 20:44:19 -0400

    Add RamerDouglasPeucker

---
+++ README.md
@@ -0,0 +1,25 @@
+# RamerDouglasPeucker.lua
+The Ramer–Douglas–Peucker algorithm takes a curve composed of line segments (connected points), and then finds a visually similar curve that requires fewer points.
+
+The module returns a single function that accepts your input and outputs the simplified set of points.
+
+### Parameters:
+- **points**
+  - Description: An array of points you wish to simplify. The points themselves can be `Vector2` objects or anything with `X` and `Y` properties (such as a simple dictionary).
+  - Type: array&lt;Vector2-like&gt;
+  - Required
+- **tolerance**
+  - Description: A number defining the degree you wish to simplify your set of points. Higher means less points, but lower quality and loss in data complexity. Lower means more points, higher quality, and higher data complexity.
+  - Type: number
+  - Optional: Default is `1`
+- **highestQuality**
+  - Description: Whether or not to skip the radial distance simplification, which will increase quality but also increases complexity.
+  - Type: boolean
+  - Optional: Default is `false`
+
+### Returns:
+  - Description: An array of simplified points, re-using a subset of the points you passed in. (If you pass in `Vector2`s, you will get `Vector2`s back; if you pass in a dictionaries with `X` and `Y` properties, you will get that back.)
+  - Type: array&lt;Vector2-like&gt;
+
+### Demo use case:
+[Simplifying player drawings to reduce instance overhead](https://gfycat.com/MemorableWiltedAnkole)
---
+++ RamerDouglasPeucker.lua
@@ -0,0 +1,100 @@
+function getSqDist(p1, p2)
+	local dx = p1.x - p2.x
+	local dy = p1.y - p2.y
+
+	return dx * dx + dy * dy
+end
+
+function getSqSegDist(p, p1, p2)
+	local x = p1.X
+	local y = p1.Y
+	local dx = p2.X - x
+	local dy = p2.y - y
+
+	if dx ~= 0 or dy ~= 0 then
+		local t = ((p.X - x) * dx + (p.y - y) * dy) / (dx * dx + dy * dy)
+
+		if t > 1 then
+			x = p2.X
+			y = p2.Y
+		elseif t > 0 then
+			x = x + dx * t
+			y = y + dy * t
+		end
+	end
+
+	dx = p.X - x
+	dy = p.Y - y
+
+	return dx * dx + dy * dy
+end
+
+function simplifyRadialDist(points, sqTolerance)
+	local prevPoint = points[1]
+	local newPoints = {prevPoint}
+	local point
+
+	for i=2, #points do
+		point = points[i]
+
+		if getSqDist(point, prevPoint) > sqTolerance then
+			table.insert(newPoints, point)
+			prevPoint = point
+		end
+	end
+
+	if prevPoint ~= point then
+		table.insert(newPoints, point)
+	end
+
+	return newPoints
+end
+
+function simplifyDPStep(points, first, last, sqTolerance, simplified)
+	local maxSqDist = sqTolerance
+	local index
+
+	for i=first+1, last do
+		local sqDist = getSqSegDist(points[i], points[first], points[last])
+
+		if sqDist > maxSqDist then
+			index = i
+			maxSqDist = sqDist
+		end
+	end
+
+	if maxSqDist > sqTolerance then
+		if index - first > 1 then
+			simplifyDPStep(points, first, index, sqTolerance, simplified)
+		end
+
+		table.insert(simplified, points[index])
+
+		if last - index > 1 then
+			simplifyDPStep(points, index, last, sqTolerance, simplified)
+		end
+	end
+end
+
+function simplifyDouglasPeucker(points, sqTolerance)
+	local last = #points
+
+	local simplified={points[1]}
+	simplifyDPStep(points, 1, last, sqTolerance, simplified)
+	table.insert(simplified, points[last])
+
+	return simplified
+end
+
+return function (points, tolerance, highestQuality)
+	if #points <= 2 then
+		return points
+	end
+
+	local sqTolerance = tolerance ~= nil and tolerance^2 or 1
+
+	points = highestQuality and points or simplifyRadialDist(points, sqTolerance)
+	points = simplifyDouglasPeucker(points, sqTolerance)
+
+	return points
+end
